d2822316c535b7bdd976aa244fdb3615f8ca9eff
Hiromu Yakura <hiromu1996@gmail.com>
2022-12-05 00:37:59 -0600

    add stream-browserify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/gis-h3-gmap'
-    : '/'
+    : '/',
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "stream": require.resolve("stream-browserify")
+      } 
+    }
+  }
 }
