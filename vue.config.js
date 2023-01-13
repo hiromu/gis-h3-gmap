1ee4a6825587f13404c6636fd8eef562163de42b
Hiromu Yakura <hiromu1996@gmail.com>
2023-01-13 17:36:09 +0900

    add a polyfill for buffer

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
+const webpack = require('webpack')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/gis-h3-gmap'
     : '/',
   configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      })
+    ],
     resolve: {
       fallback: {
+        "buffer": require.resolve("buffer"),
         "stream": require.resolve("stream-browserify")
       } 
     }
